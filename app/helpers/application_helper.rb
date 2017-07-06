af6b9f8d864faf5bf6dae9b8e447d47ddae8d10f
NTHieu95 <unknowmember1995@gmail.com>
2017-07-06 14:18:37 +0700

    chuong 345

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "rails_tutorial"
+
+    return base_title if page_title.empty?
+    page_title + " | " + base_title
+  end
 end
