b6292cc9cf92944dbff0b642b7c7974ec763966f
NTHieu95 <unknowmember1995@gmail.com>
2017-07-18 13:50:13 +0700

    Advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
@@ -21,11 +21,12 @@ class SessionsController < ApplicationController
 
   def login_success user
     log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
     redirect_to user
   end
 
   def login_fail
-    flash[:danger] = t "error_login_message"
+    flash.now[:danger] = t "error_login_message"
     render :new
   end
 end
